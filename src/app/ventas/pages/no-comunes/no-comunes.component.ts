1b9420f6d2a30f86cc7c37b9432561af5172793d
lorionj10 <armandoxd_f.o.b@hotmail.com>
2023-03-30 17:26:33 -0700

    SlicePipe, KeyValuePipe, JsonPipe, AsyncPipe

--- src/app/ventas/pages/no-comunes/no-comunes.component.ts
+++ src/app/ventas/pages/no-comunes/no-comunes.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { interval } from 'rxjs';
 
 @Component({
   selector: 'app-no-comunes',
@@ -7,8 +8,9 @@ import { Component } from '@angular/core';
 export class NoComunesComponent {
 
   // i18nSelect 
-  nombre: string = 'Edna';
-  genero: string = 'feminino';
+  nombre  : string = 'Edna';
+  genero  : string = 'femenino';
+  vuela   : boolean = false;
 
   invitacionMapa = {
     'masculino': 'invitarlo',
@@ -23,4 +25,49 @@ export class NoComunesComponent {
     '=2': 'tenemos dos clientes esperando.',
     'other': 'tenemos # clientes esperando.'
   }
+
+  cambiarCliente() {
+    this.nombre = 'Armando';
+    this.genero = 'masculino';
+  }
+
+  borrarCliente() {
+    this.clientes.pop();
+  }
+
+
+  // KeyValue Pipe
+  persona = {
+    nombre: 'Alexandra',
+    edad: 21,
+    direccion: 'Ottawa, Canada'
+  }
+
+  // JsonPipe
+  heroes = [
+    {
+      nombre: 'Superman',
+      vuela: true
+    },
+    {
+      nombre: 'Robin', 
+      vuela: false
+    },
+    {
+      nombre: 'Aquaman',
+      vuela: false
+    }
+  ]
+
+  //Async Pipe 
+  miObservable = interval(1000); // 0,1,2,3,4,5,6
+
+  valorPromesa = new Promise( (resolve, reject) => {
+
+    setTimeout(() => {
+      resolve( 'Tenemos data de promesa' );
+    }, 3500 );
+
+  });
+
 }
